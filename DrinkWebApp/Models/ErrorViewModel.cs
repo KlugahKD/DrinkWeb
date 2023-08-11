8fc256ce48bdca03a91e5b11bf95ebfbe1d586e8
Klugah <Klugah@DESKTOP-TDCC8JV>
2023-08-11 23:11:45 +0000

    Add project files.

---
+++ DrinkWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DrinkWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
