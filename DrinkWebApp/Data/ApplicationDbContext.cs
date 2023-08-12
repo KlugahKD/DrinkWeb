c6cad9f5e99f934c6bfa162db934894240676348
Klugah <Klugahdaniel1@gmail.com>
2023-08-12 23:44:17 +0000

    Created views to display the Category in the database and being able to create a new category.

--- DrinkWebApp/Data/ApplicationDbContext.cs
+++ DrinkWebApp/Data/ApplicationDbContext.cs
@@ -11,5 +11,14 @@ namespace DrinkWebApp.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+          modelBuilder.Entity<Category>().HasData(
+              new Category { Id = 1, Name = "Baileys", DisplayOrder  = 1 },
+              new Category { Id = 2, Name = "Fanta", DisplayOrder = 2 },
+              new Category { Id = 3, Name = "Coke", DisplayOrder = 3 }
+              );
+        }
     }
 }
