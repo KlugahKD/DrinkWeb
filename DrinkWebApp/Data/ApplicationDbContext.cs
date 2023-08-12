f3883f60d306ab35b8374fa21fb9c84027a76acc
Klugah <Klugahdaniel1@gmail.com>
2023-08-12 02:07:38 +0000

    Linked database and created a table using entity migrations.

---
+++ DrinkWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DrinkWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrinkWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
